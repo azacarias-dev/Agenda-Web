a3affe6debccb3140679ab120fbf70ab79feecd4
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2026-01-20 11:00:30 -0600

    Se agregaron labels

--- src/components/sections/tareas/toDoList.js
+++ src/components/sections/tareas/toDoList.js
@@ -2,12 +2,34 @@ import { TareaList } from "./storage.js";
 import { itemTarea } from "../../common/itemTarea/itemTarea.js";
 
 let tareas = () => {
+
     let sectionTareas = document.createElement("section");
     sectionTareas.className = "tareas";
+
+    let sectionLabel = document.createElement("section");
+    sectionLabel.className = "label";
 
     let h2 = document.createElement("h2");
     h2.textContent = "Lista de Tareas";
     sectionTareas.appendChild(h2);
+
+    let labelCheckList = document.createElement("h3");
+    labelCheckList.textContent = "Listo";
+    sectionLabel.appendChild(labelCheckList);
+
+    let labelDescripcion = document.createElement("h3");
+    labelDescripcion.textContent = "Descripcion";
+    sectionLabel.appendChild(labelDescripcion);
+
+    let labelFecha = document.createElement("h3");
+    labelFecha.textContent = "Fecha";
+    sectionLabel.appendChild(labelFecha);
+    
+    let labelPrioridad = document.createElement("h3");
+    labelPrioridad.textContent = "Prioridad";
+    sectionLabel.appendChild(labelPrioridad);
+
+    sectionTareas.appendChild(sectionLabel);
 
     TareaList.forEach((tarea, index) => {
         sectionTareas.appendChild(itemTarea(tarea, index));
@@ -16,4 +38,4 @@ let tareas = () => {
     return sectionTareas;
 };
 
-export {tareas};
+export { tareas };
